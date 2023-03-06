fc44eedabe07221796624336973d913772db59f1
Dustycarrot <dustycarrot8@gmail.com>
2023-03-06 13:25:36 +1300

    DONE

--- Unit-3/Assets/PlayerController.cs
+++ Unit-3/Assets/PlayerController.cs
@@ -4,24 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-private Rigidbody playerRb;
-public float jumpForce;
-public float gravityModifier;
-public bool isOnGround = true;
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isOnGround = true;
     public bool gameOver;
+    private Animator playerAnim;
+    public ParticleSystem explosionParticle;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver) {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
     private void OnCollisionEnter(Collision collision)
@@ -34,7 +38,11 @@ public bool isOnGround = true;
         {
             gameOver = true;
             Debug.Log("gameover");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
         }
+        
         
     }
 }
